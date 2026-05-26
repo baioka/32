3c47eaacaaefbbb237220d63c17f3a1d1fd2f721
baioka <bajarystanhb@gmail.com>
2026-05-26 21:46:35 +0500

    Update PersonComparator.java

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -3,7 +3,12 @@ import java.util.Comparator;
 public class PersonComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        // implement this according to your variant
-        return 0;
+        // Сначала по фамилии (по алфавиту / возрастанию)
+        int lastnameCompare = o1.lastname.compareTo(o2.lastname);
+        if (lastnameCompare != 0) {
+            return lastnameCompare;
+        }
+        // Если фамилии одинаковые, то по имени (по алфавиту / возрастанию)
+        return o1.firstname.compareTo(o2.firstname);
     }
 }
